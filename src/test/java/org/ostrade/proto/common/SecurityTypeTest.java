a84c56f395ae103e7df73c1d39e8b592772acf77
Stephen Nimmo <stephennimmo@gmail.com>
2022-08-26 08:58:06 -0500

    types and fields established

---
+++ src/test/java/org/ostrade/proto/common/SecurityTypeTest.java
@@ -0,0 +1,13 @@
+package org.ostrade.proto.common;
+
+import org.junit.jupiter.api.Test;
+import org.ostrade.proto.field.SecurityType;
+
+public class SecurityTypeTest {
+
+    @Test
+    void securityTypeEnum() {
+        SecurityType securityType = SecurityType.FUTURE;
+    }
+
+}
